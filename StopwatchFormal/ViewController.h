0f4f928317875b88149af2fa29d08d2b088aebaa
xyh3171 <xyh3171@gmail.com>
2016-05-03 11:29:11 +0800

    Initial Commit

---
+++ StopwatchFormal/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StopwatchFormal
+//
+//  Created by xuyonghua on 5/3/16.
+//  Copyright © 2016 FN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StopwatchFormal/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  StopwatchFormal
+//
+//  Created by xuyonghua on 5/3/16.
+//  Copyright © 2016 FN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
